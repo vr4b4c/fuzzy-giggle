5aefddb0f5081f91e7d3035c2c25ed18bdba1ce8
Vedran Hrncic <vedran.hrncic@infinum.hr>
2020-03-26 17:46:17 +0100

    fix share folder location

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,11 +2,16 @@
 
 set -ex
 
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
 export RAILS_ENV=production
+
+rbenv shell 2.6.1
 
 bundle install
 
-ln -s ../shared/config/master.key config/master.key
+ln -s $HOME/www/code-deploy-test/shared/config/master.key config/master.key
 
 bundle exec rake db:migrate
 bundle exec rake assets:precompile
