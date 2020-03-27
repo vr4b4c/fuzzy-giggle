35b2714246983fc546a2ddbfde2e927db755cd5e
Vedran Hrncic <vedran.hrncic@infinum.hr>
2020-03-27 12:23:44 +0100

    disable app stop

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-set -ex
+# set -ex
 
-PIDFILE=tmp/pids/server.pid
+# PIDFILE=tmp/pids/server.pid
+# echo `pwd` >> /tmp/nekaj.log
 
-if [ -f "$PIDFILE" ]; then
-  kill -SIGINT `cat $PIDFILE`
-fi
+# if [ -f "$PIDFILE" ]; then
+#   echo `cat $PIDFILE`
+#   kill -SIGINT `cat $PIDFILE`
+# fi
