652b88d22bf5f1df53a2268ad4581a40b55d68e4
Vedran Hrncic <vedran.hrncic@infinum.hr>
2020-03-27 14:43:03 +0100

    load profile before app start

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
+# why not picked up from $HOME/.bashrc?
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+cd $HOME/www/code-deploy-test/current
+
 RAILS_ENV=production bundle exec rails s -p 3000 -d
